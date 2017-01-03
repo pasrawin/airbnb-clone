6e52827d568359a048c080ea818e7026c5833f51
pasrawint <pasrawin@gmail.com>
2017-01-03 15:40:35 +0800

    added link_to listing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # root 'statics#home', as: "home"
-  root 'listings#index', as: "home"
+  root 'statics#home', as: "home"
+  
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
