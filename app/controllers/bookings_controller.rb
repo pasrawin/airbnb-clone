ee79ac74ecdfd41d8cc90660897969fbe93112b6
pasrawint <pasrawin@gmail.com>
2017-01-05 20:35:28 +0800

    improved mailing system: gem sidekiq

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,8 +6,9 @@ class BookingsController < ApplicationController
 		@booking.listing = @listing
 
 		if @booking.save
-			ReservationMailer.booking_email(current_user, @listing.user, @booking.id).deliver_now
-			flash[:success] = "Successfully booked #{@listing.title} on #{@booking.start_date} to #{@booking.end_date}"
+			ReservationJob.perform_later(current_user, @listing.user, @booking.id)
+			# ReservationMailer.booking_email(current_user, @listing.user, @booking.id).deliver_now
+			# flash[:success] = "Successfully booked #{@listing.title} on #{@booking.start_date} to #{@booking.end_date}"
 			redirect_to current_user
 		else
 			@errors = @booking.errors.full_messages
