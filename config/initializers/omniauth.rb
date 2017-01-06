2adcca9733bf53d9a9ad9582bdc1d74f7e850eca
pasrawint <pasrawin@gmail.com>
2017-01-06 10:53:03 +0800

    fixed facebook authentication

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
 #In config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
  provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET']
+
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
 end
